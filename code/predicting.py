08a515a6cafe844299e98c68a276a256c167d52a
Vnixon7 <vnixon7@outlook.com>
2022-02-02 18:52:51 -0800

    redacted info

--- code/predicting.py
+++ code/predicting.py
@@ -1,39 +1,18 @@
-from nltk.classify.naivebayes import NaiveBayesClassifier
+
 import sklearn
-from sklearn.feature_selection import chi2
-#from gensim.models.fasttext import FastText
-from xgboost import XGBClassifier
-from sklearn.multiclass import OneVsOneClassifier
-from sklearn.multiclass import OneVsRestClassifier
 from sklearn import linear_model, preprocessing
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, cross_val_score, cross_val_predict
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn.cluster import KMeans
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score, average_precision_score, roc_auc_score, confusion_matrix
-from sklearn import svm
-from sklearn.svm import SVC
 from scipy.sparse import hstack, coo_matrix
 import pandas as pd
 import numpy as np
 import pickle
 import sys
-import re
 from html.parser import HTMLParser
-from nltk.corpus import stopwords
-import nltk
-from nltk import WordPunctTokenizer
-from imblearn.over_sampling import SMOTE, RandomOverSampler
-from imblearn.under_sampling import RandomUnderSampler
 from text_cleaner import html_to_text, clean
 #nltk.download('stopwords')
 import dill
@@ -59,12 +38,12 @@ def get_app(output):
 
 def get_owner(output):
     new_output = []
-    owners = {0:"Clive Reuben",
-              1:"John Merrill",
-              2:"Kristi Bathgate",
-              3:"MaryBeth Martino",
-              4:"Michael Harrington",
-              5:"Verek Nixon"}
+    owners = {0:"Person_1",
+              1:"Person_2",
+              2:"Person_3",
+              3:"Person_4",
+              4:"Person_5",
+              5:"Person_6"}
 
     for i in output:
         new_output.append(owners[i])
@@ -84,78 +63,81 @@ def get_color(output):
         new_output.append(colors[i])
     return new_output
 
-test_data = pd.read_csv (r'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\Data\predictions1.csv')
-sp = pickle.load(open(fr'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\prod_models\model_sp1.pickle', 'rb'))
-color = pickle.load(open(fr'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\prod_models\model_color1.pickle', 'rb'))
-owner = pickle.load(open(fr'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\prod_models\model_owner1.pickle', 'rb'))
-app = pickle.load(open(fr'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\prod_models\model_app1.pickle', 'rb'))
 
-transformer1 = dill.load(open(r"C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\transformers\transformer_sp1.pickle","rb"))
-transformer2 = dill.load(open(r"C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\transformers\transformer_color1.pickle","rb"))
-transformer3 = dill.load(open(r"C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\transformers\transformer_owner1.pickle","rb"))
-transformer4 = dill.load(open(r"C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\transformers\transformer_app1.pickle","rb"))
+if __name__ == "__main__":
+    
+    test_data = pd.read_csv (r'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\Data\predictions1.csv')
+    sp = pickle.load(open(fr'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\prod_models\model_sp1.pickle', 'rb'))
+    color = pickle.load(open(fr'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\prod_models\model_color1.pickle', 'rb'))
+    owner = pickle.load(open(fr'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\prod_models\model_owner1.pickle', 'rb'))
+    app = pickle.load(open(fr'C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\prod_models\model_app1.pickle', 'rb'))
 
-
-label_maker = preprocessing.LabelEncoder()
-mm = preprocessing.MinMaxScaler()
-ss = preprocessing.StandardScaler()
-mas = preprocessing.MaxAbsScaler()
+    transformer1 = dill.load(open(r"C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\transformers\transformer_sp1.pickle","rb"))
+    transformer2 = dill.load(open(r"C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\transformers\transformer_color1.pickle","rb"))
+    transformer3 = dill.load(open(r"C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\transformers\transformer_owner1.pickle","rb"))
+    transformer4 = dill.load(open(r"C:\Users\Vnixo\OneDrive\Desktop\userStoryML\storyPointEstimation\transformers\transformer_app1.pickle","rb"))
 
 
-t_title = test_data['title']
-t_title = t_title.fillna('none')
-t_desc = test_data['description']
-t_desc = t_desc.fillna('none')
+    label_maker = preprocessing.LabelEncoder()
+    mm = preprocessing.MinMaxScaler()
+    ss = preprocessing.StandardScaler()
+    mas = preprocessing.MaxAbsScaler()
 
 
-vec_title = []
-vec_desc = []
-test_title = []
-test_desc = []
+    t_title = test_data['title']
+    t_title = t_title.fillna('none')
+    t_desc = test_data['description']
+    t_desc = t_desc.fillna('none')
 
 
-for i in range(0, len(t_title)):
-    test_title.append(list(set(clean(t_title[i]))))
-    test_desc.append(list(set(clean(t_desc[i]))))
-
-for i in range(0, len(vec_title)):
-    vec_title[i] = ','.join(vec_title[i])
-    vec_desc[i] = ','.join(vec_desc[i])
+    vec_title = []
+    vec_desc = []
+    test_title = []
+    test_desc = []
 
 
-for i in range(0, len(test_title)):
-    test_title[i] = ','.join(test_title[i])
-    test_desc[i] = ','.join(test_desc[i])
+    for i in range(0, len(t_title)):
+        test_title.append(list(set(clean(t_title[i]))))
+        test_desc.append(list(set(clean(t_desc[i]))))
 
-test_data['title'] = test_title
-test_data['description'] = test_desc
-
-tfidf_test_data = test_data[['title', 'description']].fillna('none')
-
-tfidf_test_data1 = transformer1.transform(tfidf_test_data)
-tfidf_test_data2 = transformer2.transform(tfidf_test_data)
-tfidf_test_data3 = transformer3.transform(tfidf_test_data)
-tfidf_test_data4 = transformer4.transform(tfidf_test_data)
-#tfidf_test_data5 = transformer5.transform(tfidf_test_data)
+    for i in range(0, len(vec_title)):
+        vec_title[i] = ','.join(vec_title[i])
+        vec_desc[i] = ','.join(vec_desc[i])
 
 
-sp = sp.predict(tfidf_test_data1)
-#sp2 = sp2.predict(tfidf_test_data1)
-color = get_color(color.predict(tfidf_test_data2))
-#color2 = get_color(color2.predict(tfidf_test_data5))
-owner = get_owner(owner.predict(tfidf_test_data3))
-app = get_app(app.predict(tfidf_test_data4))
+    for i in range(0, len(test_title)):
+        test_title[i] = ','.join(test_title[i])
+        test_desc[i] = ','.join(test_desc[i])
+
+    test_data['title'] = test_title
+    test_data['description'] = test_desc
+
+    tfidf_test_data = test_data[['title', 'description']].fillna('none')
+
+    tfidf_test_data1 = transformer1.transform(tfidf_test_data)
+    tfidf_test_data2 = transformer2.transform(tfidf_test_data)
+    tfidf_test_data3 = transformer3.transform(tfidf_test_data)
+    tfidf_test_data4 = transformer4.transform(tfidf_test_data)
+    #tfidf_test_data5 = transformer5.transform(tfidf_test_data)
 
 
-for i in range(0, len(sp)):
-    print(f"MODEL1 -> App: {app[i]}  Color: {color[i]}  Owner: {owner[i]}  StoryPoint: {sp[i]}")
-print("-----------")
-#for i in range(0, len(sp)):
-#    print(f"MODEL2 -> App: {app[i]}  Color: {color2[i]}  Owner: {owner[i]} StoryPoint: {sp2[i]}")
+    sp = sp.predict(tfidf_test_data1)
+    #sp2 = sp2.predict(tfidf_test_data1)
+    color = get_color(color.predict(tfidf_test_data2))
+    #color2 = get_color(color2.predict(tfidf_test_data5))
+    owner = get_owner(owner.predict(tfidf_test_data3))
+    app = get_app(app.predict(tfidf_test_data4))
+
+
+    for i in range(0, len(sp)):
+        print(f"MODEL1 -> App: {app[i]}  Color: {color[i]}  Owner: {owner[i]}  StoryPoint: {sp[i]}")
+    print("-----------")
+    #for i in range(0, len(sp)):
+    #    print(f"MODEL2 -> App: {app[i]}  Color: {color2[i]}  Owner: {owner[i]} StoryPoint: {sp2[i]}")
 
 
 
-#print(tfidf_data.head())
-#sys.exit()
+    #print(tfidf_data.head())
+    #sys.exit()
 
 
